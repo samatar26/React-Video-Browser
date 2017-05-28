80b3766aab2ee038d71a519b0632425cea04d643
Samatar Axmed <Samatar_267@hotmail.com>
2017-05-28 18:49:51 +0100

    Rendered video title and image in a list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
+import uuid from 'uuid/v4';
 
 const VideoList = ({ videos }) => {
-  return <ul className="col-md-4 list-group">{videos.length}</ul>;
+  return (
+    <ul className="col-md-4 list-group">
+      {videos.map(video => {
+        return <VideoListItem video={video} key={uuid()} />;
+      })}
+    </ul>
+  );
 };
 
 export default VideoList;
