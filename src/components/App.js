110579c4e53b34e4f9b54ef9cf35263ba0b7428e
Samatar Axmed <Samatar_267@hotmail.com>
2017-05-28 19:06:26 +0100

    First Video detail showing with iframe

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
 import VideoList from './video_list';
+import VideoDetail from './video_detail';
 
 //Free Api Key and restricted
 const API_KEY = 'AIzaSyAUD5jiHkVOCBRTl-UDPNhAUbG_GTUHwts';
@@ -21,6 +22,7 @@ class App extends Component {
       <div>
         <SearchBar />
         <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.videos[0]} />
       </div>
     );
   }
